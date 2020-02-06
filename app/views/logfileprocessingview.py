7c2cfd6f7281329a83fdca6e2c76c64a085d38ea
Jose Gallardo <jagallardo6@miners.utep.edu>
2020-02-05 21:25:20 -0700

    added checkboxes and edited Ui

--- app/views/logfileprocessingview.py
+++ app/views/logfileprocessingview.py
@@ -1,11 +1,16 @@
 import os
 
+import sys
+
 from PyQt5 import QtWidgets
 
 from .ui.uiobjects import Ui_LogFileProcessing
 
-from PyQt5.QtWidgets import QWidget, QTableWidgetItem
+from PyQt5.QtWidgets import QWidget, QTableWidgetItem, QDialog
 
+#import mainwindow
+
+root_dir = '/Users/joseg/Desktop/Spring2020/SW2/pick-tool-team07-runtime-terror/test/RootDir'
 
 class LogFileProcessingView(QWidget):
     def __init__(self, parent):
@@ -13,17 +18,17 @@ class LogFileProcessingView(QWidget):
         self.parent = parent
         self.ui = Ui_LogFileProcessing()
         self.ui.setupUi(self)
+        self.ui.logfiletable.setColumnCount(6)
+        self.ui.logfiletable.setRowCount(50)
         self.parent.setCentralWidget(self)
-        #self.exec()
         self.startProcess()
+        self.ui.pushButton.clicked.connect(self.cancelClicked)
 
     def startProcess(self):
         row = 0
         #root_dir will come from event config right?
         for dirName, subdirList, fileList in os.walk(root_dir, topdown=False):
             print('Found directory: %s' % dirName)
-            # source = QTableWidgetItem(dirName)
-            # self.ui.logfiletable.setItem(row, 1, source)
             for fname in fileList:
                 print('\t%s' % fname)
                 info = QTableWidgetItem(fname)
@@ -34,3 +39,11 @@ class LogFileProcessingView(QWidget):
                 self.ui.logfiletable.setCellWidget(row, 5, checkBox)
                 row += 1
 
+    def cancelClicked(self):
+        print("hello")
+        
+
+        
+        
+
+        
--- app/views/testGraphView.py
+++ app/views/testGraphView.py
@@ -1,7 +1,8 @@
 from .ui.uiobjects import Ui_ListGraphConfig
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QDialog, QFileDialog
+from PyQt5.QtWidgets import QDialog, QFileDialog 
+from PyQt5 import QtWidgets
 
 
 class ListGraphView(QDialog):
@@ -10,4 +11,20 @@ class ListGraphView(QDialog):
         self.parent = parent
         self.ui = Ui_ListGraphConfig()
         self.ui.setupUi(self)
+        self.ui.tableWidget.setColumnCount(11)
+        self.ui.tableWidget.setRowCount(50)
+        self.addCheckBoxes()
         self.exec_()
+
+    def addCheckBoxes(self):
+        num = 0
+        while (num < self.ui.tableWidget.columnCount()):
+            check_box = QtWidgets.QCheckBox()
+            self.ui.tableWidget.setCellWidget(0, num, check_box)
+            num += 1
+        num = 0
+        while (num <= self.ui.tableWidget.rowCount()):
+            check_box = QtWidgets.QCheckBox()
+            self.ui.tableWidget.setCellWidget(num + 1, 0, check_box)
+            num += 1
+
