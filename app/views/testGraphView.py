2ef88a48a8ee194abf9878ee3cffddc19c5e4a62
Eder Rodriguez <ederrod21@gmail.com>
2020-02-05 22:47:53 -0700

    First attempt to graph

--- app/views/testGraphView.py
+++ app/views/testGraphView.py
@@ -4,6 +4,10 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QDialog, QFileDialog 
 from PyQt5 import QtWidgets
 
+from QGraphViz.QGraphViz import QGraphViz
+from QGraphViz.DotParser import Graph
+from QGraphViz.Engines import Dot
+
 
 class ListGraphView(QDialog):
     def __init__(self, parent):
@@ -13,8 +17,41 @@ class ListGraphView(QDialog):
         self.ui.setupUi(self)
         self.ui.tableWidget.setColumnCount(11)
         self.ui.tableWidget.setRowCount(50)
+        self.setupGraph()
         self.addCheckBoxes()
         self.exec_()
+
+    def setupGraph(self): 
+        graph = self.ui.graph
+        qgv = QGraphViz(
+        # show_subgraphs=show_subgraphs,
+        # node_selected_callback=node_selected,
+        # edge_selected_callback=edge_selected,
+        # node_invoked_callback=node_invoked,
+        # edge_invoked_callback=edge_invoked,
+        )
+
+        qgv.new(Dot(Graph("Main_Graph")))
+
+        n1 = qgv.addNode(qgv.engine.graph, "Node1", label="N1")
+        n2 = qgv.addNode(qgv.engine.graph, "Node2", label="N2")
+        n3 = qgv.addNode(qgv.engine.graph, "Node3", label="N3")
+        n4 = qgv.addNode(qgv.engine.graph, "Node4", label="N4")
+        n5 = qgv.addNode(qgv.engine.graph, "Node5", label="N5")
+        n6 = qgv.addNode(qgv.engine.graph, "Node6", label="N6")
+
+        qgv.addEdge(n1, n2, {})
+        qgv.addEdge(n3, n2, {})
+        qgv.addEdge(n2, n4, {"width":2})
+        qgv.addEdge(n4, n5, {"width":4})
+        qgv.addEdge(n4, n6, {"width":5,"color":"red"})
+        qgv.addEdge(n3, n6, {"width":2})
+
+        # Build the graph (the layout engine organizes where the nodes and connections are)
+        qgv.build()
+
+        qgv.save("test.gv")
+        graph.layout().addWidget(qgv)
 
     def addCheckBoxes(self):
         num = 0
