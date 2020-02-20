a79beacb7b3f66961a800b2cfcd7174d26048997
Jose Gallardo <jagallardo6@miners.utep.edu>
2020-02-20 14:02:13 -0700

    added browse to other folders

--- app/views/dirconfigview.py
+++ app/views/dirconfigview.py
@@ -17,12 +17,27 @@ class DirConfigView(QDialog):
         self.exec_()
 
     def setupHandler(self): 
-        self.ui.browseBtn.clicked.connect(self.browse)
+        self.ui.root_browseBtn.clicked.connect(self.browse)
+        self.ui.red_browseBtn.clicked.connect(self.browse_red)
+        self.ui.blue_browseBtn.clicked.connect(self.browse_blue)
+        self.ui.white_browseBtn.clicked.connect(self.browse_white)
         self.ui.startIngestionBtn.clicked.connect(self.startIngestion)
 
     def browse(self): 
         root_dir = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
         self.ui.rootDirTxtBox.setText(root_dir)
+    
+    def browse_red(self):
+        red_dir = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+        self.ui.redFolderTxtBox.setText(red_dir)
+    
+    def browse_blue(self):
+        blue_dir = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+        self.ui.blueFolderTxtBox.setText(blue_dir)
+    
+    def browse_white(self):
+        white_dir = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+        self.ui.whiteFolderTxtBox.setText(white_dir)
     
     def startIngestion(self):
         rootDir = self.ui.rootDirTxtBox.text()
