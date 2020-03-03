7e1c9cfea03f70fe3ac3a927f6a758d517d6f2d1
Eder Rodriguez <ederrod21@gmail.com>
2020-03-02 23:10:45 -0700

    Fixing more dialogs

--- app/views/projectconfigview.py
+++ app/views/projectconfigview.py
@@ -5,7 +5,7 @@ from eventconfigmanager import EventConfigManager
 from .ui.uiobjects import Ui_TeamConfig
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QDialog, QWidget, QListWidget, QStackedWidget, QTableView, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QRadioButton, QLineEdit
+from PyQt5.QtWidgets import QDialog, QWidget, QListWidget, QStackedWidget, QTableView, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QRadioButton, QLineEdit
 
 from .eventconfigview import EventConfigView
 
@@ -15,11 +15,6 @@ class ProjectConfigView(QDialog):
         self.initUI()
 
     def initUI(self): 
-        self.viewList = QListWidget()
-        self.viewList.insertItem(0, "Team Configuration")
-        self.viewList.insertItem(1, "Directory Configuration")
-        self.viewList.insertItem(2, "Event Configuration")
-        self.viewList.insertItem(3, "Vector Configuration")
 
         self.teamConfig = QWidget()
         self.dirConfig = QWidget()
@@ -27,9 +22,19 @@ class ProjectConfigView(QDialog):
         self.vectorConfig = QWidget()
 
         self.teamConfigSetup()
+        self.dirConfigSetup()
         
         self.stack = QStackedWidget(self)
         self.stack.addWidget(self.teamConfig)
+        self.stack.addWidget(self.dirConfig)
+
+
+        self.viewList = QListWidget()
+        self.viewList.insertItem(0, "Team Configuration")
+        self.viewList.insertItem(1, "Directory Configuration")
+        self.viewList.insertItem(2, "Event Configuration")
+        self.viewList.insertItem(3, "Vector Configuration")
+        self.viewList.currentRowChanged.connect(lambda i : self.stack.setCurrentIndex(i))
 
         viewContainer = QHBoxLayout(self)
         viewContainer.addWidget(self.viewList, 30)
@@ -39,9 +44,9 @@ class ProjectConfigView(QDialog):
         self.exec_()
     
     def teamConfigSetup(self): 
-        self.viewLabel = QLabel()
-        self.viewLabel.setText("Team Configuration")
-        self.viewLabel.setAlignment(Qt.AlignHCenter|Qt.AlignVCenter)
+        viewLabel = QLabel()
+        viewLabel.setText("Team Configuration")
+        viewLabel.setAlignment(Qt.AlignHCenter|Qt.AlignVCenter)
 
         self.leadRbtn = QRadioButton()
         self.leadRbtn.setText("Lead")
@@ -64,15 +69,87 @@ class ProjectConfigView(QDialog):
         self.connectBtn.clicked.connect(self.connect)
 
 
-        self.teamViewContainer = QVBoxLayout()
-        self.teamViewContainer.addWidget(self.viewLabel)
-        self.teamViewContainer.addWidget(self.leadRbtn)
-        self.teamViewContainer.addLayout(self.ipContainer)
-        self.teamViewContainer.addWidget(self.connectionsLbl)
-        self.teamViewContainer.addWidget(self.connectionsView)
-        self.teamViewContainer.addWidget(self.connectBtn)
+        teamViewContainer = QVBoxLayout()
+        teamViewContainer.addWidget(viewLabel)
+        teamViewContainer.addWidget(self.leadRbtn)
+        teamViewContainer.addLayout(self.ipContainer)
+        teamViewContainer.addWidget(self.connectionsLbl)
+        teamViewContainer.addWidget(self.connectionsView)
+        teamViewContainer.addWidget(self.connectBtn)
 
-        self.teamConfig.setLayout(self.teamViewContainer)
+        self.teamConfig.setLayout(teamViewContainer)
+
+    def dirConfigSetup(self): 
+        viewLabel = QLabel()
+        viewLabel.setText("Directory Configuration")
+        viewLabel.setAlignment(Qt.AlignHCenter|Qt.AlignVCenter)
+        viewLabelCon = QHBoxLayout()
+        viewLabelCon.addWidget(viewLabel)
+
+        # Root directory controls
+        rootDirLbl = QLabel("Root Directory")
+        self.rootDirPath = QLineEdit()
+        self.rootDirBrowse = QPushButton()
+        self.rootDirBrowse.setText("Browse")
+        
+
+        # Root Directory controls container
+        rootDirContainer = QHBoxLayout()
+        rootDirContainer.addWidget(rootDirLbl)
+        rootDirContainer.addWidget(self.rootDirPath)
+        rootDirContainer.addWidget(self.rootDirBrowse)
+
+        # Red Folder controls
+        redFolderLbl = QLabel("Red Team Folder")
+        self.redFolderPath = QLineEdit()
+        self.redFolderBrowse = QPushButton()
+        self.redFolderBrowse.setText("Browse")
+        self.redFolderBrowse.setEnabled(False)
+
+        # Red folder controls container
+        redContainer = QHBoxLayout()
+        redContainer.addWidget(redFolderLbl)
+        redContainer.addWidget(self.redFolderPath)
+        redContainer.addWidget(self.redFolderBrowse)
+
+        # Blue Folder controls
+        blueFolderLbl = QLabel("Blue Team Folder")
+        self.blueFolderPath = QLineEdit()
+        self.blueFolderBrowse = QPushButton()
+        self.blueFolderBrowse.setText("Browse")
+        self.blueFolderBrowse.setEnabled(False)
+
+        # Blue folder controls container
+        blueContainer = QHBoxLayout()
+        blueContainer.addWidget(blueFolderLbl)
+        blueContainer.addWidget(self.blueFolderPath)
+        blueContainer.addWidget(self.blueFolderBrowse)
+
+        # Blue Folder controls
+        whiteFolderLbl = QLabel("Blue Team Folder")
+        self.whiteFolderPath = QLineEdit()
+        self.whiteFolderBrowse = QPushButton()
+        self.whiteFolderBrowse.setText("Browse")
+        self.whiteFolderBrowse.setEnabled(False)
+
+        # Blue folder controls container
+        whiteContainer = QHBoxLayout()
+        whiteContainer.addWidget(whiteFolderLbl)
+        whiteContainer.addWidget(self.whiteFolderPath)
+        whiteContainer.addWidget(self.whiteFolderBrowse)
+
+        self.dirConfigSave = QPushButton("Save")
+        
+
+        dirViewContainer = QVBoxLayout()
+        dirViewContainer.addLayout(viewLabelCon)
+        dirViewContainer.addLayout(rootDirContainer)
+        dirViewContainer.addLayout(redContainer)
+        dirViewContainer.addLayout(blueContainer)
+        dirViewContainer.addLayout(whiteContainer)
+        dirViewContainer.addWidget(self.dirConfigSave)
+
+        self.dirConfig.setLayout(dirViewContainer)
 
     def connect(self): 
         #TODO
