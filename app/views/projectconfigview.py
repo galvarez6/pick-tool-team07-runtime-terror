802fcd348188f0b12b15abf1f2ad18fbee203940
Jose Gallardo <jagallardo6@miners.utep.edu>
2020-03-02 23:13:19 -0700

    EventConfig View

--- app/views/projectconfigview.py
+++ app/views/projectconfigview.py
@@ -5,7 +5,7 @@ from eventconfigmanager import EventConfigManager
 from .ui.uiobjects import Ui_TeamConfig
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QDialog, QWidget, QListWidget, QStackedWidget, QTableView, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QRadioButton, QLineEdit
+from PyQt5.QtWidgets import QDialog, QWidget, QListWidget, QStackedWidget, QTableView, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QRadioButton, QLineEdit, QDateTimeEdit
 
 from .eventconfigview import EventConfigView
 
@@ -27,9 +27,11 @@ class ProjectConfigView(QDialog):
         self.vectorConfig = QWidget()
 
         self.teamConfigSetup()
+        self.eventConfigSetup()
         
         self.stack = QStackedWidget(self)
         self.stack.addWidget(self.teamConfig)
+        self.stack.addWidget(self.eventConfig)
 
         viewContainer = QHBoxLayout(self)
         viewContainer.addWidget(self.viewList, 30)
@@ -77,6 +79,48 @@ class ProjectConfigView(QDialog):
     def connect(self): 
         #TODO
         pass
+        #self.eventConfigSetup()
 
     def setupHandlers(self): 
         self.ui.connBtn.clicked.connect(self.connect)
+
+    def eventConfigSetup(self):
+        self.viewLabel = QLabel()
+        self.viewLabel.setText("Event Configuration")
+        self.viewLabel.setAlignment(Qt.AlignHCenter|Qt.AlignVCenter)
+
+        self.eventNameLbl = QLabel()
+        self.eventNameLbl.setText("Event Name")
+        self.eventName = QLineEdit()
+
+        self.eventDescriptionLbl = QLabel()
+        self.eventDescriptionLbl.setText("Event Description")
+        self.eventDescription = QLineEdit()
+
+        self.startTimeLbl = QLabel()
+        self.startTimeLbl.setText("Start Date and Time")
+        self.startTime = QDateTimeEdit()
+
+        self.endTimeLbl = QLabel()
+        self.endTimeLbl.setText("End Date and Time")
+        self.endTime = QDateTimeEdit()
+
+        self.saveEventBtn = QPushButton()
+        self.saveEventBtn.setText("Save Event") 
+
+        self.eventConfigContainer = QVBoxLayout()
+        self.eventConfigContainer.addWidget(self.viewLabel)
+        self.eventConfigContainer.addWidget(self.eventNameLbl)
+        self.eventConfigContainer.addWidget(self.eventName)
+        self.eventConfigContainer.addWidget(self.eventDescriptionLbl)
+        self.eventConfigContainer.addWidget(self.eventDescription)
+        self.eventConfigContainer.addWidget(self.startTimeLbl)
+        self.eventConfigContainer.addWidget(self.startTime)
+        self.eventConfigContainer.addWidget(self.endTimeLbl)
+        self.eventConfigContainer.addWidget(self.endTime)
+        self.eventConfigContainer.addWidget(self.saveEventBtn)
+
+        self.eventConfig.setLayout(self.eventConfigContainer)
+
+
+
